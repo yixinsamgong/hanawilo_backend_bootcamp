f9a082639b8f27c3745694bc5476c5abec8305b4
yixinsamgong <sam19931225@gmail.com>
2023-03-12 18:43:17 -0400

    finish step 3

--- music/controllers/songController.js
+++ music/controllers/songController.js
@@ -13,15 +13,39 @@ const createSong = (req, res, next) => {
     ` })
 }
 
-const deleteSong = (req, res, next) => {
+const deleteSongs = (req, res, next) => {
     res
     .status(200)
     .setHeader('Content-Type', 'application/json')
     .json({ message: 'Deleting all the songs!' })
 }
 
+const getSong = (req, res, next) => {
+    res
+    .status(200)
+    .setHeader('Content-Type', 'application/json')
+    .json({ message: `Show me the Song with cateogry Id of ${req.params.songId}` })
+}
+
+const putSong = (req, res, next) => {
+    res
+    .status(200)
+    .setHeader('Content-Type', 'application/json')
+    .json({ message: `Update the Song with cateogry Id of ${req.params.SongId}` })
+}
+
+const deleteSong = (req, res, next) => {
+    res
+    .status(200)
+    .setHeader('Content-Type', 'application/json')
+    .json({ message: `Delete the Song with cateogry Id of ${req.params.SongId}` })
+}
+
 module.exports = {
     createSong, 
-    deleteSong, 
-    getSongs
+    deleteSongs, 
+    getSongs,
+    getSong,
+    putSong,
+    deleteSong
 }
--- music/routes/song.js
+++ music/routes/song.js
@@ -3,13 +3,21 @@ const router = express.Router();
 
 const {
     createSong, 
+    deleteSongs, 
+    getSongs,
+    getSong,
+    putSong, 
     deleteSong, 
-    getSongs
 } = require('../controllers/songController');
 
 router.route('/')
     .get(getSongs)
     .post(createSong)
+    .delete(deleteSongs)
+
+router.route('/:songId')
+    .get(getSong)
+    .post(putSong)
     .delete(deleteSong)
 
 module.exports = router;
